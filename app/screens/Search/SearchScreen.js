77676edd02283e1393b9132a32887aa5d8897b38
Alejandro Fleitas <afleitasinfo@gmail.com>
2020-06-12 18:05:27 -0300

    Creación de los 4 screen principales del proyecto, Bares, MiCuenta, Buscador, Top Bares

--- app/screens/Search/SearchScreen.js
+++ app/screens/Search/SearchScreen.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 // create a component
-const BarsScreen = () => {
+const SearchScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>Estamos en Bars</Text>
+            <Text>Estamos en SearchScreen</Text>
         </View>
     );
 };
@@ -22,4 +22,4 @@ const styles = StyleSheet.create({
 });
 
 //make this component available to the app
-export default BarsScreen;
+export default SearchScreen;
--- app/screens/TopBars/TopBarsScreen.js
+++ app/screens/TopBars/TopBarsScreen.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 // create a component
-const BarsScreen = () => {
+const TopBarsScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>Estamos en Bars</Text>
+            <Text>Estamos en TopBarsScreen</Text>
         </View>
     );
 };
@@ -22,4 +22,4 @@ const styles = StyleSheet.create({
 });
 
 //make this component available to the app
-export default BarsScreen;
+export default TopBarsScreen;
