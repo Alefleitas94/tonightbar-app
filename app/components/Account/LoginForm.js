b6d18fcd44c9de73281dec3177dbba3a08315111
Alejandro Fleitas <afleitasinfo@gmail.com>
2020-07-01 19:15:37 -0300

    Agregando funcionalidad de login con firebase a LoginForm

--- app/components/Account/LoginForm.js
+++ app/components/Account/LoginForm.js
@@ -3,44 +3,48 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Button, Input, Icon } from "react-native-elements";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import Loading from '../../components/ActivityIndicator/Loading';
-
-
+import Loading from "../../components/ActivityIndicator/Loading";
+import * as firebase from "firebase";
+import {useNavigation} from '@react-navigation/native';
 
 //utils
-import {validateEmail} from '../../utils/Validation';
+import { validateEmail } from "../../utils/Validation";
 
 // create a component
 const LoginForm = (props) => {
-  
-  const {toastRef} = props;
-
+  const { toastRef } = props;
+  const navigation = useNavigation();
   //Hide Password
   const [hidePassword, setHidePassword] = useState(true);
 
-//Visible Loading
-const [isVisible, setIsVisible] = useState(false)
+  //Visible Loading
+  const [isVisible, setIsVisible] = useState(false);
 
   //State Inputs
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  
-  const login = () => {
-     setIsVisible(true);
-     if ((!email) || (!password)){
-      console.log('Todos los campos son obligatorios');
-      toastRef.current.show('Todos los campos son obligatorios', 2000)
-     }else {
-       if (!validateEmail(email)) {
-        toastRef.current.show('El email es incorrecto');
-       }else {
-         //Logica Firebase
-         console.log('Login correcto');
-        
-       }
-     }
-     setIsVisible(false);
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    setIsVisible(true);
+    if (!email || !password) {
+      console.log("Todos los campos son obligatorios");
+      toastRef.current.show("Todos los campos son obligatorios", 2000);
+    } else {
+      if (!validateEmail(email)) {
+        toastRef.current.show("El correo es incorrecto");
+      } else {
+        //Logica Firebase
+        await firebase
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+          .then(() => {console.log("login correcto");
+            navigation.navigate('MyAccount');
+        })
+          .catch(() => {toastRef.current.show("Correo y/o contraseña incorrecta.")});
+      }
+    }
+    setIsVisible(false);
+  };
 
   return (
     <View style={styles.formContainer}>
@@ -75,9 +79,9 @@ const [isVisible, setIsVisible] = useState(false)
         containerStyle={styles.btnContainerLogin}
         title="Ingresar"
         buttonStyle={styles.btnLogin}
-        onPress= {login}
+        onPress={login}
       />
-       <Loading text= "Iniciando sesion" isVisible={isVisible} />
+      <Loading text="Iniciando sesion" isVisible={isVisible} />
     </View>
   );
 };
@@ -98,7 +102,7 @@ const styles = StyleSheet.create({
   btnContainerLogin: {
     marginTop: 20,
     width: "95%",
-    marginHorizontal: 5
+    marginHorizontal: 5,
   },
   btnLogin: {
     backgroundColor: "#3390db",
